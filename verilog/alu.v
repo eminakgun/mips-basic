91adb2d04a3d61173dcb881aefa143b6381b9e49
eminakgun <eminakgn@outlook.com>
2024-01-19 23:59:42 +0300

    Convert zero_bit into structural

--- verilog/alu.v
+++ verilog/alu.v
@@ -6,7 +6,7 @@ module alu (
     input [2:0] alu_ctr
 );
 
-    // ALU with CLA from submodule
+    // ALU with CLA from prev project
     alu_cla alu_cla_i(/* Clk */,
                       /* Reset */, 
                         alu_src1, alu_src2, alu_ctr,
@@ -14,11 +14,25 @@ module alu (
                         alu_result, 
                         /* We */);
 
-    always @(*) begin
+    // zero-bit behavirol
+    /* always @(*) begin
         zero_bit = 1'b0;
         if (alu_result == 0) begin
             zero_bit = 1'b1;
         end
-    end
+    end */
+
+    // Structural
+    wire or_result;
+    or or_result_i(or_result, alu_result[0],  alu_result[1],  alu_result[2],  alu_result[3], 
+                              alu_result[4],  alu_result[5],  alu_result[6],  alu_result[7], 
+                              alu_result[8],  alu_result[9],  alu_result[10], alu_result[11], 
+                              alu_result[12], alu_result[13], alu_result[14], alu_result[15], 
+                              alu_result[16], alu_result[17], alu_result[18], alu_result[19], 
+                              alu_result[20], alu_result[21], alu_result[22], alu_result[23], 
+                              alu_result[24], alu_result[25], alu_result[26], alu_result[27], 
+                              alu_result[28], alu_result[29], alu_result[30], alu_result[31]);
+    not zero_bit_i(zero_bit, or_result);
+    
     
 endmodule
